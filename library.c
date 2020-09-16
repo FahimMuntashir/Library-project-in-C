813a76d880a6b72e243b9589482b2bb22cddfdcc
FahimMuntashir <fahim.tech.me@gmail.com>
2020-09-17 03:09:58 +0600

    heading design

---
+++ library.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void password();
+void heading();
+
+int main()
+{
+    password();
+}
+
+void heading()
+{
+    printf("\n\n\t**************Library Management Software*************\n\n");
+    // fflush(stdin);
+        getchar();
+
+}
+
+void password()
+{
+    heading();
+}
